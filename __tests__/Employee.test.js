26afc43a09fbeda802a9b6cf075af1788102aa17
jacob m <jacobamaynard@gmail.com>
2023-01-28 23:23:52 -0600

    start employee.test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,10 +1,23 @@
 const Employee = require("../lib/Employee");
 
+test("creating a new employee", () => {
+    const employ = new Employee();
+    expect(typeof(employ)).toBe("object")
+})
+
 test("does the name value work", () => {
-    const jacob = new Employee(jacob);
-    expect(typeof(employee.name)).toBe("jacob")
+    const employ = new Employee(jacob);
+    expect(typeof(employ.name)).toBe("jacob")
 })
 
 test("does the id value work", () => {
-    const 1 = new Employee()
+    const employ = new Employee(jacob, id)
+    expect(typeof(employ.id)).toBe(id)
 })
+
+test("does the email value work", () => {
+    const employ = new Employee(jacob, 2, email)
+    expect(typeof(employ.email)).toBe(email)
+})
+
+test("")
